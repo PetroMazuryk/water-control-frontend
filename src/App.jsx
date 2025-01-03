9510c3012cb7064dd21e530eeedef849d8e51b30
PetroMazuryk <109526725+PetroMazuryk@users.noreply.github.com>
2025-01-03 18:40:23 +0200

    add routes to App.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,47 @@
-import Logo from './components/Logo/Logo';
+import { Route, Routes } from 'react-router-dom';
+import SharedLayout from './components/SharedLayout/SharedLayout';
+import PrivateRoute from './components/PrivateRoute';
+import RestrictedRoute from './components/RestrictedRoute';
+
 function App() {
   return (
-    <div>
-      <Logo />
-    </div>
+    <SharedLayout>
+      <Routes>
+        <Route path="/" element={<div>HomePage</div>} />
+
+        <Route
+          path="/register"
+          element={
+            <RestrictedRoute
+              redirectTo={`/water/${Date.now()}`}
+              component={<div>RegisterPage</div>}
+            />
+          }
+        />
+
+        <Route
+          path="/login"
+          element={
+            <RestrictedRoute
+              redirectTo={`/water/${Date.now()}`}
+              component={<div>LoginPage</div>}
+            />
+          }
+        />
+
+        <Route
+          path="/water/:date"
+          element={
+            <PrivateRoute
+              redirectTo="/login"
+              component={<div>WaterControlPage</div>}
+            />
+          }
+        ></Route>
+
+        <Route path="*" element={<div>NotFoundPage </div>} />
+      </Routes>
+    </SharedLayout>
   );
 }
 export default App;
