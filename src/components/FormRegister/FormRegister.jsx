65ccdabb6b55afe0aa01389fa900e19db634cd18
PetroMazuryk <109526725+PetroMazuryk@users.noreply.github.com>
2025-01-04 17:00:08 +0200

    add a registration form

---
+++ src/components/FormRegister/FormRegister.jsx
@@ -0,0 +1,172 @@
+import { useState } from 'react';
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as Yup from 'yup';
+import sprite from '../../assets/sprite.svg';
+
+import css from './FormRegister.module.css';
+
+const FormRegister = () => {
+  const isLoading = false;
+  const [showPassword, setShowPassword] = useState(false);
+  const [showPasswordRepeat, setshowPasswordRepeat] = useState(false);
+
+  const schemaValidation = Yup.object({
+    email: Yup.string()
+      .required('Потрібна електронна адреса')
+      .email('Недійсна адреса електронної пошти'),
+    password: Yup.string()
+      .required('Потрібен пароль')
+      .min(7, 'Пароль має містити принаймні 7 символів')
+      .matches(
+        /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/,
+        'Пароль повинен містити принаймні одну літеру та одну цифру'
+      ),
+    repeatPassword: Yup.string()
+      .oneOf([Yup.ref('password')], 'Паролі мають збігатися')
+      .required('Будь ласка, підтвердьте свій пароль'),
+  });
+
+  const togglePasswordVisibility = () => {
+    setShowPassword(!showPassword);
+  };
+
+  const togglePasswordRepeatVisibility = () => {
+    setshowPasswordRepeat(!showPasswordRepeat);
+  };
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({ resolver: yupResolver(schemaValidation) });
+
+  const onSubmitForm = (data) => {
+    console.log(data);
+  };
+
+  return (
+    <div className={css.registerComponent}>
+      <form onSubmit={handleSubmit(onSubmitForm)}>
+        <h2 className={css.registerTitle}>Реєстрація</h2>
+        <div className={css.registerForm}>
+          <label className={css.registerLabel}>
+            Електронна пошта
+            <input
+              className={
+                errors.email?.message
+                  ? `${css.registerInputError}`
+                  : `${css.registerInput}`
+              }
+              {...register('email')}
+              placeholder="Введіть свою пошту"
+            />
+            {errors.email?.message ? (
+              <p className={css.registerErrorMessage}>
+                {errors.email?.message}
+              </p>
+            ) : (
+              ''
+            )}
+          </label>
+
+          <label className={css.registerLabel}>
+            <span>Пароль</span>
+            <span className={css.registerPassword}>
+              <input
+                type={showPassword ? 'text' : 'password'}
+                className={
+                  errors.password?.message
+                    ? `${css.registerInputError}`
+                    : `${css.registerInput}`
+                }
+                {...register('password')}
+                placeholder="Введіть свій пароль"
+              />
+              <button
+                className={css.passwordIconBtn}
+                type="button"
+                onClick={togglePasswordVisibility}
+              >
+                {showPassword === false ? (
+                  <svg className={css.passwordIcon}>
+                    <use xlinkHref={sprite + '#icon-eye-off'} />
+                  </svg>
+                ) : (
+                  <svg className={css.passwordIcon}>
+                    <use xlinkHref={sprite + '#icon-eye'} />
+                  </svg>
+                )}
+              </button>
+            </span>
+            {errors.password?.message ? (
+              <p className={css.registerErrorMessage}>
+                {errors.password?.message}
+              </p>
+            ) : (
+              ''
+            )}
+          </label>
+
+          <label className={css.registerLabel}>
+            <span>Повторіть пароль</span>
+            <span className={css.registerPassword}>
+              <input
+                type={showPasswordRepeat ? 'text' : 'password'}
+                className={
+                  errors.repeatPassword?.message
+                    ? `${css.registerInputError}`
+                    : `${css.registerInput}`
+                }
+                {...register('repeatPassword')}
+                placeholder="Повторіть пароль"
+              />
+              <button
+                className={css.passwordIconBtn}
+                type="button"
+                onClick={togglePasswordRepeatVisibility}
+              >
+                {showPasswordRepeat === false ? (
+                  <svg className={css.passwordIcon}>
+                    <use xlinkHref={sprite + '#icon-eye-off'} />
+                  </svg>
+                ) : (
+                  <svg className={css.passwordIcon}>
+                    <use xlinkHref={sprite + '#icon-eye'} />
+                  </svg>
+                )}
+              </button>
+            </span>
+            {errors.repeatPassword?.message ? (
+              <p className={css.registerErrorMessage}>
+                {errors.repeatPassword?.message}
+              </p>
+            ) : (
+              ''
+            )}
+          </label>
+        </div>
+        {isLoading ? (
+          <div className={css.registerLoader}>
+            <div height={44} width={44}>
+              Loading ...
+            </div>
+            .
+          </div>
+        ) : (
+          <div className={css.registerButtonsContainer}>
+            <button
+              disabled={isLoading && true}
+              className={css.registerBtn}
+              type="submit"
+            >
+              Зареєструватися
+            </button>
+          </div>
+        )}
+      </form>
+    </div>
+  );
+};
+
+export default FormRegister;
--- src/pages/RegisterPage/RegisterPage.jsx
+++ src/pages/RegisterPage/RegisterPage.jsx
@@ -1,12 +1,14 @@
 import AdvantagesSection from '../../components/AdvantagesSection/AdvantagesSection';
 import FormWrapper from '../../components/FormWrapper/FormWrapper';
+import FormRegister from '../../components/FormRegister/FormRegister';
+
 import css from './RegisterPage.module.css';
 
 const RegisterPage = () => {
   return (
     <div className={css.registerPage}>
       <FormWrapper>
-        <div>RegisterForm</div>
+        <FormRegister />
         <div>RegisterFormFooter</div>
       </FormWrapper>
 
