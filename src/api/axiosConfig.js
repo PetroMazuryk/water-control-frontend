06445cda457761d1f15b09beca1ec7fdba8b4941
PetroMazuryk <109526725+PetroMazuryk@users.noreply.github.com>
2025-01-06 16:41:36 +0200

    add function fetchRefreshToken

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,7 +1,39 @@
 import axios from 'axios';
 
+const BASE_URL = import.meta.env.VITE_API_URL;
+
 const instance = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: BASE_URL,
 });
+
+let store;
+export const injectStore = (_store) => {
+  store = _store;
+};
+
+export const fetchRefreshToken = async () => {
+  const { data } = await axios.post(
+    `${BASE_URL}/users/refresh`,
+    {},
+    { withCredentials: true }
+  );
+  return data;
+};
+
+instance.interceptors.request.use(
+  (config) => {
+    if (store) {
+      const state = store.getState();
+      const token = state?.auth?.token;
+      if (token) {
+        config.headers['Authorization'] = `Bearer ${token}`;
+      }
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
 
 export default instance;
