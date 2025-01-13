fdbfcb340fdf3f4be653b5e836cbe7229eb64d28
PetroMazuryk <109526725+PetroMazuryk@users.noreply.github.com>
2025-01-13 22:38:32 +0200

    add username rendering based on condition

--- src/components/UserPanel/UserPanel.jsx
+++ src/components/UserPanel/UserPanel.jsx
@@ -2,15 +2,21 @@ import UserBar from '../UserBar/UserBar.jsx';
 import styles from './UserPanel.module.css';
 import { useSelector } from 'react-redux';
 import { selectUserName } from '../../redux/auth/selectors.js';
+import { selectUserEmail } from '../../redux/auth/selectors.js';
 
 const UserPanel = () => {
   const userName = useSelector(selectUserName);
+  const userEmail = useSelector(selectUserEmail);
+
+  const emailUsername = userEmail.split('@')[0];
+
+  const displayName = userName === 'User' ? emailUsername : userName;
 
   return (
     <div className={styles.userPanel}>
       <h1 className={styles.userPanelTitle}>
         Привіт,
-        <span className={styles.userPanelTitleName}>{userName}</span>
+        <span className={styles.userPanelTitleName}>{displayName}</span>
       </h1>
       <UserBar name={userName} />
     </div>
--- src/redux/auth/selectors.js
+++ src/redux/auth/selectors.js
@@ -3,6 +3,7 @@ export const selectToken = (state) => state.auth.token;
 export const selectIsLoggedIn = (state) => state.auth.isLoggedIn;
 
 export const selectUserName = (state) => state.auth.user.name;
+export const selectUserEmail = (state) => state.auth.user.email;
 
 export const selectIsAuthenticated = (state) => {
   const token = selectToken(state);
