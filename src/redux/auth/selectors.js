eb7ea3317bcdb0c6dcae9b76b343aa39f78c679a
PetroMazuryk <109526725+PetroMazuryk@users.noreply.github.com>
2025-01-17 22:42:15 +0200

    add user form settings

--- src/redux/auth/selectors.js
+++ src/redux/auth/selectors.js
@@ -2,6 +2,7 @@ export const selectIsLoading = (state) => state.auth.isLoading;
 export const selectToken = (state) => state.auth.token;
 export const selectIsLoggedIn = (state) => state.auth.isLoggedIn;
 
+export const selectUser = (state) => state.auth.user;
 export const selectUserName = (state) => state.auth.user.name;
 export const selectUserEmail = (state) => state.auth.user.email;
 
