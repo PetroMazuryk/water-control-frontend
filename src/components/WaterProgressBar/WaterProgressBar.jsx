13c4512407657f61546c24b98981470d65b1edb7
PetroMazuryk <109526725+PetroMazuryk@users.noreply.github.com>
2025-01-24 18:43:56 +0200

    add display conditions in the component water progress bar

--- src/components/WaterProgressBar/WaterProgressBar.jsx
+++ src/components/WaterProgressBar/WaterProgressBar.jsx
@@ -1,71 +1,24 @@
-// import { selectWaterRate } from '../../redux/auth/selectors';
-import { selectWaterDailyRecord } from '../../redux/water/selectors';
-import { useSelector } from 'react-redux';
 import clsx from 'clsx';
+import { useSelector } from 'react-redux';
+import SelectDate from '../SelectDate/SelectDate';
+import { selectWaterDailyRecord } from '../../redux/water/selectors';
 
 import css from './WaterProgressBar.module.css';
-import SelectDate from '../SelectDate/SelectDate';
 
-// const WaterProgressBar = () => {
-//   const currentWater = useSelector((state) => state.water.todayAmount.value);
-
-//   const aim = useSelector(selectWaterRate);
-
-//   const calculatedPercentage = Math.round((currentWater / (aim * 1000)) * 100);
-
-//   const percentage = calculatedPercentage >= 100 ? 100 : calculatedPercentage;
-//   const isBelowAim = currentWater < aim * 1000;
-
-//   return (
-//     <div className={css.progressBarContainer}>
-//       <h2 className={css.progressBarTitle}>Сьогодні</h2>
-//       <div className={css.progressBar}>
-//         <div
-//           className={clsx(css.progressBarFill, {
-//             [css.belowAim]: isBelowAim,
-//             [css.aboveAim]: !isBelowAim,
-//           })}
-//           style={{
-//             width: `${percentage}%`,
-//           }}
-//         >
-//           <p
-//             className={clsx(css.percentNumber, {
-//               [css.belowAimRate]: isBelowAim,
-//               [css.aboveAimRate]: !isBelowAim,
-//             })}
-//           >
-//             {isBelowAim ? `${percentage.toFixed(0)}%` : 'норма'}
-//           </p>
-//           <div
-//             className={clsx(css.ball, {
-//               [css.belowAim]: isBelowAim,
-//               [css.aboveAim]: !isBelowAim,
-//             })}
-//           ></div>
-//         </div>
-//       </div>
-//       <div className={css.percentContainer}>
-//         <p className={css.percent}>0%</p>
-//         <p className={css.percent}>50%</p>
-//         <p className={css.percent}>100%</p>
-//       </div>
-//     </div>
-//   );
-// };
-// =======================
-// // export default WaterProgressBar;
 const WaterProgressBar = () => {
-  // Отримати дані через селектор
   const data = useSelector(selectWaterDailyRecord);
 
-  // Функція для розрахунку сумарного відсотка+++++=====
   const calculateTotalPercentage = (data) => {
-    if (!data || data.length === 0) return 0; // Перевірка на випадок, якщо масив порожній
-    return data.reduce((total, entry) => total + entry.percentage, 0);
+    if (!data || data.length === 0) return 0;
+    return data.reduce((total, entry) => {
+      const percentage = entry.percentage || 0;
+      return total + percentage;
+    }, 0);
   };
 
   const totalPercentage = calculateTotalPercentage(data);
+  const isBelowAim = totalPercentage < 100;
+  const clampedPercentage = Math.min(Math.max(totalPercentage, 0), 100);
 
   return (
     <div className={css.progressBarContainer}>
@@ -75,23 +28,29 @@ const WaterProgressBar = () => {
       <div className={css.progressBar}>
         <div
           className={clsx(css.progressBarFill, {
-            [css.belowAim]: totalPercentage < 100,
-            [css.aboveAim]: totalPercentage >= 100,
+            [css.belowAim]: isBelowAim,
+            [css.aboveAim]: !isBelowAim,
           })}
           style={{
-            width: `${totalPercentage > 100 ? 100 : totalPercentage}%`,
+            width: `${clampedPercentage}%`,
           }}
         >
           <p
             className={clsx(css.percentNumber, {
-              [css.belowAimRate]: totalPercentage < 100,
-              [css.aboveAimRate]: totalPercentage >= 100,
+              [css.belowAimRate]: isBelowAim,
+              [css.aboveAimRate]: !isBelowAim,
             })}
           >
             {totalPercentage >= 100
               ? 'Норма'
               : `${totalPercentage.toFixed(0)}%`}
           </p>
+          <div
+            className={clsx(css.ball, {
+              [css.belowAim]: totalPercentage < 100,
+              [css.aboveAim]: totalPercentage >= 100,
+            })}
+          ></div>
         </div>
       </div>
       <div className={css.percentContainer}>
