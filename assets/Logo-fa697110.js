a5f570fb833d4994e896b07e7a485381039dc9ca
PetroMazuryk <109526725+PetroMazuryk@users.noreply.github.com>
2025-01-30 21:44:57 +0000

    Deploying to gh-pages from @ PetroMazuryk/water-control-frontend@1783e4dba0c05992543ceb4658e99c1315590bb0 🚀

--- assets/Logo-fa697110.js
+++
@@ -1,1 +0,0 @@
-import{j as o,L as t}from"./index-1c5e8504.js";const a="_logo_1eccw_1",s="_animate_1eccw_1",e={logo:a,animate:s},c=()=>o.jsx(t,{to:"/",className:e.logo,"data-text":"WATERCONTROL",children:"WaterControl"});export{c as L};
