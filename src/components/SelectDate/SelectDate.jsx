fcf2119ac04ec9c19e96feca49ebdd5feb1a0b26
PetroMazuryk <109526725+PetroMazuryk@users.noreply.github.com>
2025-02-14 15:44:10 +0200

    add a language change to the select date component

--- src/components/SelectDate/SelectDate.jsx
+++ src/components/SelectDate/SelectDate.jsx
@@ -1,15 +1,17 @@
-import { useParams } from 'react-router-dom';
 import clsx from 'clsx';
+import { useParams } from 'react-router-dom';
+import { useTranslation } from 'react-i18next';
 import { parseDateTime } from '../../helpers/parseDate.js';
 import { getDateMonthString } from '../../helpers/getDateMonthString.js';
 
 import css from './SelectDate.module.css';
 
 const SelectDate = ({ className }) => {
+  const { t } = useTranslation();
   const { date } = useParams();
   const { isToday, day, month } = getDateMonthString(parseDateTime(date));
 
-  const dateString = isToday ? 'Сьогодні' : `${day} ${month}`;
+  const dateString = isToday ? t('today') : `${day} ${month}`;
   return <p className={clsx(css.selectedDate, className)}>{dateString}</p>;
 };
 export default SelectDate;
