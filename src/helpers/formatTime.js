77403ad9fa1d64f6d3d83581dc315d992bf97803
PetroMazuryk <109526725+PetroMazuryk@users.noreply.github.com>
2025-02-21 15:58:44 +0200

    change the time format display depending on the selected language

--- src/helpers/formatTime.js
+++ src/helpers/formatTime.js
@@ -1,14 +1,21 @@
+import i18next from 'i18next';
+
 export const formatTime = (date) => {
   const dateObj = new Date(Number(date));
-  let hours = dateObj.getHours();
+  const hours = dateObj.getHours();
   const minutes = dateObj.getMinutes();
-  const ampm = hours >= 12 ? 'PM' : 'AM';
-  if (hours >= 12) {
-    hours = hours % 12;
-    hours = hours ? hours : 12;
-  }
 
-  const hoursStr = hours.toString().padStart(2, '0');
-  const minutesStr = minutes.toString().padStart(2, '0');
-  return `${hoursStr}:${minutesStr} ${ampm}`;
+  const isEnglish = i18next.language.startsWith('en');
+
+  if (isEnglish) {
+    const ampm = hours >= 12 ? 'PM' : 'AM';
+    let hours12 = hours % 12 || 12;
+    return `${hours12.toString().padStart(2, '0')}:${minutes
+      .toString()
+      .padStart(2, '0')} ${ampm}`;
+  } else {
+    return `${hours.toString().padStart(2, '0')}:${minutes
+      .toString()
+      .padStart(2, '0')}`;
+  }
 };
