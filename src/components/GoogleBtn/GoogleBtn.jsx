ef07382ce548951ddc52adfc529ec425c6de46c4
PetroMazuryk <109526725+PetroMazuryk@users.noreply.github.com>
2025-04-24 00:20:12 +0300

    add Google button translation

--- src/components/GoogleBtn/GoogleBtn.jsx
+++ src/components/GoogleBtn/GoogleBtn.jsx
@@ -1,9 +1,13 @@
+import { useTranslation } from 'react-i18next';
+
 import css from './GoogleBtn.module.css';
 
 const GoogleBtn = () => {
+  const { t } = useTranslation();
+
   return (
     <a className={css.btn} href="http://localhost:3000/users/google">
-      Продовжити з Google
+      {t('googleBtn')}
     </a>
   );
 };
