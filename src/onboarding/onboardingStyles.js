67be94b1b6fc2df620be5830f0bbd2d58e950ed7
PetroMazuryk <109526725+PetroMazuryk@users.noreply.github.com>
2025-05-16 23:33:44 +0300

    add styles theme

--- src/onboarding/onboardingStyles.js
+++ src/onboarding/onboardingStyles.js
@@ -1,4 +1,4 @@
-import { disableBodyScroll, enableBodyScroll } from "body-scroll-lock";
+import { disableBodyScroll, enableBodyScroll } from 'body-scroll-lock';
 
 export const disableBody = () => disableBodyScroll(document.body);
 export const enableBody = () => enableBodyScroll(document.body);
@@ -9,6 +9,8 @@ export const onboardingStyles = {
     borderRadius: 10,
     marginLeft: 10,
     marginRight: 10,
+    background: 'var(--white)',
+    color: 'var(--color-font-main)',
   }),
 
   close: (base) => ({
@@ -18,9 +20,6 @@ export const onboardingStyles = {
     right: '13px',
     cursor: 'pointer',
     fontSize: '20px',
-    color: 'var(--color-font-main)',  
+    color: 'var(--color-font-main)',
   }),
-  
-
-  
 };
