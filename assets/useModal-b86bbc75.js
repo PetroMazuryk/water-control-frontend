7246198bf211ebd2ddc187c9546fc086c85064f5
PetroMazuryk <109526725+PetroMazuryk@users.noreply.github.com>
2025-05-28 21:49:27 +0000

    Deploying to gh-pages from @ PetroMazuryk/water-control-frontend@2d29a86e45df201cb7ed7e6eec898812de6b16ae 🚀

---
+++ assets/useModal-b86bbc75.js
@@ -0,0 +1,1 @@
+import{r as o,$ as t}from"./index-2c8f3c6c.js";const a=()=>o.useContext(t);export{a as u};
--- assets/useModal-cecfcba2.js
+++
@@ -1,1 +0,0 @@
-import{r as o,$ as t}from"./index-64ba38fb.js";const a=()=>o.useContext(t);export{a as u};
